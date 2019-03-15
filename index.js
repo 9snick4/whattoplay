ceb5e13ddd911a402838915f6deef1409150b6f0
9snick4 <nic17014@byui.edu>
2019-03-15 15:51:07 +0100

    also readded getrate assignment

--- index.js
+++ index.js
@@ -10,9 +10,49 @@ app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/',  (req,res) => res.render('pages/nights', {result: listNight()}))
+  .get('/getList',  (req,res) => res.render('pages/nights', {result: listNight()}))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/form.html')))
+  .get('/getRate', (req,res) => res.render('pages/result', {result: calculateRate(req.query.letter, req.query.weight)}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+  function calculateRate(mailType, weight) {
+    switch (mailType) {
+      case 's':
+        if (weight < 1) {
+          return 0.55;
+        } else if (weight < 2) {
+          return 0.70;
+        } else if (weight < 3) {
+          return 0.85;
+        } else if (weight < 3.5) {
+          return 1.00;
+        } else return "Not applicable";
+      case 'm':
+        if (weight < 1) {
+          return 0.50;
+        } else if (weight < 2) {
+          return 0.65;
+        } else if (weight < 3) {
+          return 0.80;
+        } else if (weight < 3.5) {
+          return 0.95;
+        } else return "Not applicable";  
+      case 'f':
+        if (weight < 13) {
+          return 1 + (0.15 * (Math.floor(weight) - 1))
+        } else return "Not applicable";
+     case 'r':
+        if (weight < 4) {
+          return 3.66;
+        } else if (weight < 8) {
+          return 4.39;
+        } else if (weight < 12) {
+          return 5.19;
+        } else if (weight < 13) {
+          return 5.71;
+        } else return "Not applicable";
+    }
+}
   
 function listNight(req, res) {
   

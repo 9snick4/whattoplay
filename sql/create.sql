1bd7420df2f352074f52b5189e8ccf6fd9714084
9snick4 <nic17014@byui.edu>
2019-03-15 12:13:09 +0100

    sql code

---
+++ sql/create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE night (
+    nightid SERIAL PRIMARY KEY,
+    hostusername VARCHAR(50) NOT NULL,
+    nightname VARCHAR(100),
+    hostname VARCHAR(50),
+    day DATE
+);
+
+CREATE TABLE choice (
+    choiceid SERIAL PRIMARY KEY,
+    nightid INT NOT NULL REFERENCES night(nightid),
+    gameid INT NOT NULL,
+    gamername VARCHAR(50)
+);
+
---
+++ sql/insert.sql
@@ -0,0 +1,73 @@
+INSERT INTO night (
+    hostusername,
+    nightname,
+    hostname,
+    day
+) VALUES (
+ '9snick4',
+ 'tester game night 1',
+ 'Stefano Nicotra',
+ '10/19/2019'
+);
+
+INSERT INTO night (
+    hostusername,
+    nightname,
+    hostname,
+    day
+) VALUES (
+ '9snick4',
+ 'tester game night 2',
+ 'Stefano Nicotra',
+ '10/16/2019'
+);
+
+INSERT INTO choice (
+    nightid,
+    gameid,
+    gamername
+) VALUES (
+ 1,
+ 230802,
+ 'Fanny Nicotra'
+);
+
+INSERT INTO choice (
+    nightid,
+    gameid,
+    gamername
+) VALUES (
+ 1,
+ 230802,
+ 'Fanny Nicotra'
+);
+
+INSERT INTO choice (
+    nightid,
+    gameid,
+    gamername
+) VALUES (
+ 1,
+ 194655,
+ 'Mattia Nicotra'
+);
+
+INSERT INTO choice (
+    nightid,
+    gameid,
+    gamername
+) VALUES (
+ 2,
+ 194594,
+ 'Davide Nicotra'
+);
+
+INSERT INTO choice (
+    nightid,
+    gameid,
+    gamername
+) VALUES (
+ 2,
+ 2655,
+ 'Fabio Nicotra'
+);
